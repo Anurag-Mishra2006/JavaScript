549048ee94caddcc29bca3cb7ced3ff7777811f3
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-11-24 17:14:57 +0530

    first commit

--- 01_Basic/script.js
+++ 01_Basic/script.js
@@ -1,4 +1,6 @@
-alert("hello world");
+
+console.log("I am now entering in this world of web")
+alert("I am in the system...");
 console.log("code is running  ")
 var a = prompt("Enter your number ")
 console.log("your number is "+a)
