0925493f22daab36d158182d7f07f05f2328a55f
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-11-26 22:53:42 +0530

    first commit

--- 01_Basic/07_Memory.js
+++ 01_Basic/07_Memory.js
@@ -4,12 +4,19 @@ let myYo = "Anurag"
 let anothername = myYo
 console.log(anothername)
 
-
+//  stack ,me copy milta h 
 anothername = "Mishra ji "
-console.log(myYo)
-console.log(anothername)
+console.log(myYo) // go to stack 
+console.log(anothername) // go to stack
 
-let user1 = {
+let user1 = { // this go in heap memory
     emial : "am61223044@gmail.com",
     upi: "User@yble"
 }
+
+let user2= user1
+user2.emial =  "Anurag@gmail.com" 
+
+console.log(user1.emial) // heap me reference milta h
+//  due to reference the change occurs in the original value 
+console.log(user2.emial)
