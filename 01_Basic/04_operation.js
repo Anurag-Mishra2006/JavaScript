51150570f55775aa2c83c5f1248a6847d9a17c27
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-11-26 19:17:23 +0530

    first commit

---
+++ 01_Basic/04_operation.js
@@ -0,0 +1,33 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2) // give remainder.
+let str1 = "Hello"
+let str2 = " Anurag"
+let str3 = str1 +str2
+console.log(str3)
+  
+console.log("1"+4)
+console.log(5 + "5")
+console.log("1"+4+8) // yha pe string ki tarah kaam hoga
+console.log(5 +8 + "5") //yaha pe 5+8 ho raha h and then string 
+console.log(+true)
+console.log(+"") // when convert empty paranthesis to integer it gave value of 0
+
+let num1 , num2 , num3
+num1 = num2  = nums3 = 9*5
+console.log(num1)
+
+let gameCounter = 100
+gameCounter++; //postfix 
+console.log(gameCounter)
+++gameCounter; //prefix
+console.log(gameCounter)
+
+// link to study
+//  https://262.ecma-international.org/14.0/index.html#sec-ecmascript-data-types-and-values
+//  You can also visit mdn for datatype
---
+++ 01_Basic/07_Memory.js
@@ -0,0 +1,15 @@
+// Memory are of two type 1. Stack  2.Heap
+// stack(Primitive)          , Heap(Non-primative)
+let myYo = "Anurag"
+let anothername = myYo
+console.log(anothername)
+
+
+anothername = "Mishra ji "
+console.log(myYo)
+console.log(anothername)
+
+let user1 = {
+    emial : "am61223044@gmail.com",
+    upi: "User@yble"
+}
