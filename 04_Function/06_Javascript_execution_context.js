66ccad3f7440aa1fc4682a534dcf6a6a2db9369e
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-11-29 10:58:38 +0530

    first commit

---
+++ 04_Function/06_Javascript_execution_context.js
@@ -0,0 +1,27 @@
+//  on the brower ka global execution context( this) ==> window
+// js is single threaded 
+// -->  global execution context 
+// --> functional execution context
+// --> eval execution context
+
+
+/**
+ * the execution of js code is in the two phase 
+ *              1. Memory  Creation Phase
+ *              2. Execution Phase
+ * 
+ */
+
+
+/**
+ * now lets see how the js code execute 
+ *          1. Global Execution ---> this
+ *          2. Memory Creation Phase ...// isme variable ko space allote hota h 
+ *          3. Execution Phase ... // isme variable ki value assigned hoti h 
+ *          
+ */
+
+
+// +++++++++++++ call stack +++++++++++
+//  LIFO ==> last in first out
+ 
